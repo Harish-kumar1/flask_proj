4455140188070a011d47f01020be2e89ab330064
Harish-kumar1 <harishkumarss05@gmail.com>
2021-02-10 19:04:55 +0530

    Adding basic button functionalities

--- flask_proj.py
+++ flask_proj.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash
+from entry_values import item_values
+
 app = Flask(__name__)
 
-@app.route("/")
+app.config["SECRET_KEY"] = 'bbc13112232fa4c77becbd0d799e40d5'
+
+@app.route("/", methods = ['GET', 'POST'])
 def hello_world():
-    return render_template('index.html')
+    form = item_values()
+    
+    return render_template('index.html', form = form)
  
 
 if __name__ == '__main__':
